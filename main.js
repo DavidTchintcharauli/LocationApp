576816ed0cb78528ca43eab9cc6460d9f5ebd82b
DavidTchintcharauli <chincharaulidavid@gmail.com>
2024-03-21 17:03:02 +0400

    Updated feature getLocation

--- main.js
+++ main.js
@@ -1,50 +1,55 @@
 import './style.css';
-import {Map, View} from 'ol';
+import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
+import { fromLonLat } from 'ol/proj';
 
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: new OSM(),
+    }),
   ],
   view: new View({
     center: [0, 0],
-    zoom: 2
-  })
+    zoom: 2,
+  }),
 });
 
-locationButton.addEventListener('click', function() {
-  // Check if the Geolocation API is supported by the browser
-  if ('geolocation' in navigator) {
-    // Use the Geolocation API to get the user's current position
-    navigator.geolocation.getCurrentPosition(function(position) {
-      // Retrieve the latitude and longitude coordinates
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
+const locationButton = document.getElementById('locationButton');
 
-      var userLocation = new ol.Feature({
-        geometry: new ol.geom.Point(ol.proj.fromLonLat([longitude, latitude]))
+locationButton.addEventListener('click', function () {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      const userLocation = new Feature({
+        geometry: new Point(fromLonLat([longitude, latitude])),
       });
 
-      var userLocationLayer = new ol.layer.Vector({
-        source: new ol.source.Vector({
-          features: [userLocation]
+      const userLocationLayer = new VectorLayer({
+        source: new VectorSource({
+          features: [userLocation],
         }),
-        style: new ol.style.Style({
-          image: new ol.style.Circle({
+        style: new Style({
+          image: new CircleStyle({
             radius: 6,
-            fill: new ol.style.Fill({ color: 'blue' }),
-            stroke: new ol.style.Stroke({ color: 'white', width: 2 })
-          })
-        })
+            fill: new Fill({ color: 'blue' }),
+            stroke: new Stroke({ color: 'white', width: 2 }),
+          }),
+        }),
       });
 
       map.addLayer(userLocationLayer);
 
-      map.getView().setCenter(ol.proj.fromLonLat([longitude, latitude]));
+      map.getView().setCenter(fromLonLat([longitude, latitude]));
     });
   } else {
     alert('Geolocation is not supported by your browser.');
